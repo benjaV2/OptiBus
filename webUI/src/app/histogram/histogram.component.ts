24c2e46e605154bb2d6332ce6bfd0e5d9eccb4f0
binyamin bendor <binyamin.bendor@bonditglobal.com>
2020-08-10 00:26:55 +0300

    making progress

--- webUI/src/app/histogram/histogram.component.ts
+++ webUI/src/app/histogram/histogram.component.ts
@@ -10,18 +10,18 @@ export class HistogramComponent implements OnInit {
 
   data;
   factor = 1;
-  labels = ["24","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
+  labels = ["0","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    const headers = new HttpHeaders()
-      .append('Content-Type', 'application/json')
-      .append('Access-Control-Allow-Headers', 'Content-Type')
-      .append('Access-Control-Allow-Methods', 'GET')
-      .append('Access-Control-Allow-Origin', '*');
+    // const headers = new HttpHeaders()
+    //   .append('Content-Type', 'application/json')
+    //   .append('Access-Control-Allow-Headers', 'Content-Type')
+    //   .append('Access-Control-Allow-Methods', 'GET')
+    //   .append('Access-Control-Allow-Origin', '*');
     
     let url = "http://benja.com:8000/data"
-    this.http.get(url, {headers}).subscribe(      
+    this.http.get(url).subscribe(
       res => {
         //debugger;
         this.data = res;        
